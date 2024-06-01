7bb7399ed3fbdf40688557d0cd577ae3507c095a
Hakeem <126789780+HoopStar23@users.noreply.github.com>
2024-06-01 10:21:10 +0200

    Pantalla Actividades Detalles

--- RaftRush/src/DetallesActividad.java
+++ RaftRush/src/DetallesActividad.java
@@ -1,6 +1,7 @@
 import com.formdev.flatlaf.themes.FlatMacDarkLaf;
 
 import javax.swing.*;
+import javax.swing.border.Border;
 import java.awt.*;
 
 public class DetallesActividad extends JFrame{
@@ -11,19 +12,23 @@ public class DetallesActividad extends JFrame{
     private JButton btnReserva;
     private JPanel jplDatosReserva;
     private JLabel lblTitulo;
-    private JLabel lblFoto;
     private JLabel lblDescripcion;
     private JLabel lblPrecio;
-    private JLabel lblGradient;
+    private JLabel lblFoto;
 
     public DetallesActividad(){
         setContentPane(jplGeneral);
+        btnFecha.putClientProperty("JButton.buttonType", "roundRect");
+        btnReserva.putClientProperty("JButton.buttonType", "roundRect");
+        //comboCantidad.putClientProperty("JComponent.roundRect", true);
 
         background();
     }
     private void background(){
         ImageIcon background = new ImageIcon("resources/imagenes/Montanyas.png");
         lblBG.setIcon(background);
+        ImageIcon roundAct = new ImageIcon("resources/imagenes/FotoActividad.png");
+        lblFoto.setIcon(roundAct);
     }
 
     public static void main(String[] args) {
@@ -31,12 +36,18 @@ public class DetallesActividad extends JFrame{
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
-                JFrame frame = new DetallesActividad();
-                frame.setSize(660,1033);
-                frame.setResizable(false);
-                frame.setLocationRelativeTo(null);
-                frame.setVisible(true);
+                createWindow();
             }
         });
+    }
+
+    private static void createWindow(){
+        JFrame frame = new DetallesActividad();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(660,1033);
+        frame.setResizable(false);
+        frame.setLocationRelativeTo(null);
+
+        frame.setVisible(true);
     }
 }
