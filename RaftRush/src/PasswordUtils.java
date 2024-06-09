30462b8d4f6a306f1a8dfc48e2fb0d1b2123bd78
Maël Grandguillot <150013128+MaelGM@users.noreply.github.com>
2024-06-09 12:45:30 +0200

    Documentación de la encriptacion de la contraseña

--- RaftRush/src/PasswordUtils.java
+++ RaftRush/src/PasswordUtils.java
@@ -5,7 +5,15 @@ import java.util.Base64;
 
 
 public class PasswordUtils {
-    // Generar un hash para la contraseña
+
+    /**
+     * La función hashPassword toma una contraseña, la combina con un valor aleatorio (salt), y luego genera un hash seguro utilizando SHA-256.
+     * El resultado es una cadena que contiene tanto el salt como el hash de la contraseña, separados por :, lo que permite almacenar
+     * y verificar la contraseña de manera segura.
+     * @param password Contraseña que se debe encriptar
+     * @return Cadena que contiene tanto el salt como el hash de la contraseña, separados por :.
+     * @throws NoSuchAlgorithmException
+     */
     public static String hashPassword(String password) throws NoSuchAlgorithmException {
         // Crear un salt
         byte[] salt = getSalt();
@@ -19,7 +27,13 @@ public class PasswordUtils {
         return Base64.getEncoder().encodeToString(salt) + ":" + Base64.getEncoder().encodeToString(hashedPassword);
     }
 
-    // Verificar si la contraseña ingresada coincide con el hash almacenado
+    /**
+     * La función checkPassword verifica si una contraseña ingresada es correcta comparándola con un hash almacenado.
+     * @param password Contraseña sin encriptar
+     * @param storedHash Contraseña encriptada
+     * @return True si ambas son iguales, o false en caso contrario
+     * @throws NoSuchAlgorithmException
+     */
     public static boolean checkPassword(String password, String storedHash) throws NoSuchAlgorithmException {
         String[] parts = storedHash.split(":");
         byte[] salt = Base64.getDecoder().decode(parts[0]);
@@ -34,7 +48,10 @@ public class PasswordUtils {
         return MessageDigest.isEqual(testHash, hash);
     }
 
-    // Generar un salt seguro
+    /**
+     * El método getSalt genera un salt aleatorio de 16 bytes usando SecureRandom
+     * @return Salt aleatorio de 16 bytes usando SecureRandom
+     */
     private static byte[] getSalt() {
         SecureRandom sr = new SecureRandom();
         byte[] salt = new byte[16];
